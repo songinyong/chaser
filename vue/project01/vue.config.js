f7e572c01b7219c4dd6d4d2f9c556bfe34d4b065
songinyong <ttyy441@naver.com>
2022-09-08 12:17:31 +0900

    [UPDATE] eslint 무시

--- vue/project01/vue.config.js
+++ vue/project01/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  useEslint: false,
+  lintOnSave: false,
   devServer: {
     // 'auto' | 'all' [string] here
     allowedHosts: "all"
