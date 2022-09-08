f608bb6f024431205d22039ea35ccb5c20bcb6ce
songinyong <ttyy441@naver.com>
2022-09-08 12:10:12 +0900

    [UPDATE] 도메인 업데이트

--- vue/project01/vue.config.js
+++ vue/project01/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  useEslint: false,
   devServer: {
     // 'auto' | 'all' [string] here
     allowedHosts: "all"
